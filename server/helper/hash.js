b1ea5c4ec4bf7cd3cdfb8536253ce7de9576d1c3
luthfyhakim <luthfyhakim250404@gmail.com>
2022-06-07 15:15:41 +0700

    hash password and jwt

---
+++ server/helper/hash.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcryptjs');
+const SALT = 10; // jmlh huruf nya
+
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, SALT);
+}
+
+const comparePassword = (password, password_db) => {
+  return bcrypt.compareSync(password, password_db);
+}
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+}
